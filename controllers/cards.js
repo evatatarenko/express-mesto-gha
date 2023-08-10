888ad28c02a6abdea651ab1b5790cdaf67fb9b21
evatatarenko <evatatarenko@yandex.ru>
2023-08-10 18:46:06 +0300

    fix 9

--- controllers/cards.js
+++ controllers/cards.js
@@ -23,30 +23,6 @@ const getCards = (req, res) => {
     .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
-// const deleteCard = (req, res) => {
-//   try {
-//     Card.findByIdAndRemove(req.params.cardId)
-//       .then((card) => {
-//         if (!card) {
-//           throw new Error('NotFoundError');
-//         }
-//         res.send({ data: card });
-//       })
-//       .catch((err) => {
-//         console.log(err.name);
-//         if (err.name === 'CastError') {
-//           res.status(400).send({ message: 'Переданы некорректные данные' });
-//         } else {
-//           res.status(404).send({ message: 'Карточка не найдена' });
-//         }
-//       });
-//   } catch (err) {
-//     if (req.params.cardId.length !== 24) {
-//       res.status(400).send({ message: 'Переданы некорректные данные' });
-//     }
-//   }
-// };
-
 const deleteCard = async (req, res) => {
   try {
     if (!mongoose.Types.ObjectId.isValid(req.params.cardId)) {
@@ -71,43 +47,6 @@ const deleteCard = async (req, res) => {
     }
   }
 };
-
-// const addLike = (req, res) => {
-//   try {
-//     Card.findByIdAndUpdate(
-//       req.params.cardId,
-//       { $addToSet: { likes: req.user._id } },
-//       { new: true },
-//     )
-//       .then((card) => {
-//         console.log(req.params.cardId);
-//         if (!card) {
-//           throw new Error('NotFoundError');
-//         }
-//         if (req.params.cardId.length !== 24) {
-//           throw new Error('Неверный id');
-//         }
-//         res.send({ data: card });
-//       })
-//       .catch((err) => {
-//         if (err.name === 'CastError') {
-//           res.status(400).send({ message: 'Переданы некорректные данные' });
-//         } else if (err.name === 'ValidationError') {
-//           res.status(400).send({ message: 'Переданы некорректные данные' });
-//         } else if (err.message === 'NotFoundError') {
-//           res.status(404).send({ message: 'Карточка не найдена' });
-//         } else if (err.message === 'Неверный id') {
-//           console.log(err.message);
-//           res.status(400).send({ message: 'Переданы некорректные данные' });
-//         } else {
-//           console.log(err.name);
-//           res.status(500).send({ message: 'На сервере произошла ошибка' });
-//         }
-//       });
-//   } catch (err) {
-//     res.status(400).send({ message: 'Переданы некорректные данные' });
-//   }
-// };
 
 const addLike = async (req, res) => {
   try {
@@ -139,42 +78,6 @@ const addLike = async (req, res) => {
     }
   }
 };
-
-// const deleteLike = (req, res) => {
-//   try {
-//     Card.findByIdAndUpdate(
-//       req.params.cardId,
-//       { $pull: { likes: req.user._id } },
-//       { new: true },
-//     )
-//       .then((card) => {
-//         console.log(req.params.cardId);
-//         if (req.params.cardId.length !== 24) {
-//           throw new Error('Неверный id');
-//         } else if (!card) {
-//           throw new Error('NotFoundError');
-//         }
-//         res.send({ data: card });
-//       })
-//       .catch((err) => {
-//         if (err.name === 'CastError' && req.params.cardId.length !== 24) {
-//           res.status(400).send({ message: 'Переданы некорректные данные' });
-//         } else if (err.name === 'CastError') {
-//           res.status(404).send({ message: 'Карточка не найдена' });
-//         } else if (err.name === 'ValidationError') {
-//           res.status(400).send({ message: 'Переданы некорректные данные' });
-//         } else if (err.message === 'NotFoundError') {
-//           res.status(404).send({ message: 'Карточка не найдена' });
-//         } else if (err.message === 'Неверный id') {
-//           res.status(400).send({ message: 'Переданы некорректные данные' });
-//         } else {
-//           res.status(500).send({ message: 'На сервере произошла ошибка' });
-//         }
-//       });
-//   } catch (err) {
-//     res.status(400).send({ message: 'Переданы некорректные данные' });
-//   }
-// };
 
 const deleteLike = async (req, res) => {
   try {
