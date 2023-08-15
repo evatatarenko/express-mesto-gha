ae4d7456937b22e8d5e0a5ab3ffad6dcb72e9d9d
evatatarenko <evatatarenko@yandex.ru>
2023-08-15 17:34:19 +0300

    fix 7

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,9 @@
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET = 'hghghghghghghghg' } = process.env;
+
+console.log(JWT_SECRET)
 
 const getUser = (req, res, next) => {
   const { userId } = req.params;
